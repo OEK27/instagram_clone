7c7344957cb29f5ce337e06bd0cf07db39c52558
Orhan Emre Kaya <105721156+DevilCoder23@users.noreply.github.com>
2022-12-03 18:42:41 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/signup_screen.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDbQB-69HVF8auWWe9IAy9V4mqz8fKf15Q",
+          appId: "1:184339696283:web:67307e75e4d62738d1143e",
+          messagingSenderId: "184339696283",
+          projectId: "instagramflutter-4df39",
+          storageBucket: "instagramflutter-4df39.appspot.com"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      // home: const ResponsiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home: const SignupScreen(),
+    );
+  }
+}
